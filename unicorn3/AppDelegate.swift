0f9ebd2f0caef47ae7543de766af0366f7426b5a
rangwei <rangwei@gmail.com>
2019-08-13 21:11:33 +0800

    + README

--- unicorn3/AppDelegate.swift
+++ unicorn3/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        UINavigationBar.applyFioriStyle()
+        
         // Override point for customization after application launch.
         let splitViewController = window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
---
+++ unicorn3/README.md
@@ -0,0 +1,10 @@
+#  Global Unicorn List App for iOS
+
+## Design language: SAP Fiori
+[SAP Fiori](https://experience.sap.com/fiori-design/)
+
+## Powered by SAP CP SDK for iOS
+[Apple](https://developer.apple.com/sap/)
+
+[SAP](https://developers.sap.com/topics/cloud-platform-sdk-for-ios.html)
+
